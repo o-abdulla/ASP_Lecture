8d17b4bd32fe63326cc39221ba590f11e7fe62c4
Omar Abdulla <osabdulla@gmail.com>
2023-08-09 17:22:33 -0400

    First

---
+++ ASP_Lecture/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using ASP_Lecture.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ASP_Lecture.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["Name"] = "Omar Abdulla";
+            ViewBag.Age = 34;
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+        //public IActionResult Result(string CatName, int CatAge)
+        //{
+        //    ViewData["CatName"] = CatName;
+        //    ViewData["CatAge"] = CatAge;
+        //    return View();
+        //}
+
+        public IActionResult Result(Cat c)
+        {
+            return View(c);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ASP_Lecture/Models/Cat.cs
@@ -0,0 +1,9 @@
+﻿namespace ASP_Lecture.Models
+{
+    public class Cat
+    {
+        public string CatName { get; set; }
+        public int CatAge { get; set; }
+        public bool Fixed { get; set; }
+    }
+}
